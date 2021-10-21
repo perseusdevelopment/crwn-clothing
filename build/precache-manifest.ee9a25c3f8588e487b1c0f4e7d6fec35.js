9b667200c09af4876f7a9ce42998bc56734a188b
Brett Warnebold <68746756+perseusdevelopment@users.noreply.github.com>
2021-10-21 13:19:30 -0400

    Add files via upload

---
+++ build/precache-manifest.ee9a25c3f8588e487b1c0f4e7d6fec35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e519e60ef1dc83c7ddcd0a5e6c23986e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c814c8d981695d5c4ba",
+    "url": "/static/css/main.6fb8b348.chunk.css"
+  },
+  {
+    "revision": "4cbebd961b25ff5f9321",
+    "url": "/static/js/2.4bff3e3d.chunk.js"
+  },
+  {
+    "revision": "8c814c8d981695d5c4ba",
+    "url": "/static/js/main.7ef86c99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
